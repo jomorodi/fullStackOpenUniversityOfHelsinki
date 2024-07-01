a125e444f9f5a22cb085c67c20cc283d0cfe8590
jeffery omorodion <omorojeff@gmail.com>
2024-07-01 19:37:34 +0300

    had to redo the part1.2 again because doing part 1.1 undid changes

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,16 +1,11 @@
+import Part from './Part'
 
 export default function Content ({part1 , exercises1, part2 , exercises2, part3 , exercises3 }) {
   return (
     <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part = {part1} exercise={exercises1} />
+      <Part part = {part2} exercise={exercises2} />
+      <Part part = {part3} exercise={exercises3} />
     </div>
   );
 }
