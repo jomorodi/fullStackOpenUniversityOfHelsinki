dfdc78a24a82dd87c746a6630beca38f60016d8c
jeffery omorodion <omorojeff@gmail.com>
2024-07-01 16:08:55 +0300

    first working version

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './Header.jsx'
+import Content from './Content.jsx'
+import Total from './Total.jsx'
+
+function App() {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} />
+      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
+    </div>
+  )
+}
+
+export default App
---
+++ part1/courseinfo/src/Content.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Content ({part , exercise})
+{
+
+
+    return ( <p>
+        {part} {exercise}
+      </p>);
+
+}
+
