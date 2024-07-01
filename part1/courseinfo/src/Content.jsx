9921a352955e8ac3064f882668389e2d39646998
jeffery omorodion <omorojeff@gmail.com>
2024-07-01 19:26:09 +0300

    solution to part 1.1

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,11 +1,16 @@
-import Part from './Part'
 
 export default function Content ({part1 , exercises1, part2 , exercises2, part3 , exercises3 }) {
   return (
     <div>
-      <Part part= {part1}  exercise= {exercises1}/>
-      <Part part= {part2}  exercise= {exercises2}/>
-      <Part part= {part3}  exercise= {exercises3}/>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
     </div>
   );
 }
