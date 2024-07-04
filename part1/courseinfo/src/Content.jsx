82dcdf7d404c3d1ab08aaadd3b06fd99bb8991a9
jeffery omorodion <omorojeff@gmail.com>
2024-07-04 19:09:08 +0300

    commit for excercise  1.3

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,11 +1,11 @@
 import Part from './Part'
 
-export default function Content ({part1 , exercises1, part2 , exercises2, part3 , exercises3 }) {
+export default function Content ({part1 , part2 ,  part3  }) {
   return (
     <div>
-      <Part part = {part1} exercise={exercises1} />
-      <Part part = {part2} exercise={exercises2} />
-      <Part part = {part3} exercise={exercises3} />
+      <Part part = {part1.name} exercise={part1.exercises} />
+      <Part part = {part2.name} exercise={part2.exercises} />
+      <Part part = {part3.name} exercise={part3.exercises} />
     </div>
   );
 }
