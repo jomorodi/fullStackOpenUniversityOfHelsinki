0650aa9b3c64ed55beee41e358ad60e03133d6e3
jeffery omorodion <omorojeff@gmail.com>
2024-07-23 19:42:15 +0300

    solution to part 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import PersonForm from './PersonForm'
 import Filter from './Filter'
 import Persons from './Persons'
-
+import axios from 'axios'
 
 
 
@@ -15,12 +15,13 @@ const DisplayPersons = ({persons}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+
+  useEffect (() => {
+    const promise = axios.get('http://localhost:3001/persons')
+    promise.then ((response) => { setPersons(response.data) })
+
+   }, [])
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('') ;
   const [newNumber, setNewNumber] = useState('') ;
   const [filterWord, setFilterWord] = useState('');
